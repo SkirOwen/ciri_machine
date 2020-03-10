76db91d9acfd38120cce75541514b5e1c4854201
Owen <owen.allemang@gmail.com>
2020-03-10 14:10:03 +0100

    creation of project with python and R

---
+++ imports.py
@@ -0,0 +1,14 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import seaborn as sb
+import pandas as pd
+import prince
+import inspect
+import itertools as it
+import operator as op
+import os
+import random
+import re
+import string
+import sys
---
+++ main.R
@@ -0,0 +1,5 @@
+# Title     : CIRI_Machine_Learning
+# Objective :
+# Created by: owen
+# Created on: 10/03/2020
+
