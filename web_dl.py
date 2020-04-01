9ab38b01a506d3cf191645b016b89bbf68004c3a
Owen <owen.allemang@gmail.com>
2020-04-01 18:02:19 +0200

    script to dl sr

--- web_dl.py
+++ web_dl.py
@@ -25,12 +25,12 @@ for link in soup.find_all("a"):
 situation_reports_links = list(dict.fromkeys(situation_reports_links))
 
 for sr in situation_reports_links:
-	sr_name = situation_reports_links[0].split("/")[-1].split("?")[0]
+	sr_name = sr.split("/")[-1].split("?")[0]
 	available_sr_name.append(sr_name)
-
+	
 k = 0
 
-for dl_link, name, i in zip(situation_reports_links, available_sr_name, range(len(available_sr_name), -1)):
+for dl_link, name, i in zip(situation_reports_links, available_sr_name, range(len(available_sr_name), 0, -1)):
 	FILE_DIR = os.path.join(DATASET_DIR, name)
 	if not os.path.exists(FILE_DIR):
 		
@@ -40,7 +40,7 @@ for dl_link, name, i in zip(situation_reports_links, available_sr_name, range(le
 		with open(FILE_DIR, "wb") as f:
 			f.write(report.content)
 			
-		print("Situation report - ", i+1, "downloaded as ", name)
+		print("Situation report - ", i, "downloaded as ", name)
 		k += 1
 		
 print("%d situation reports downloaded" % k)
