6ce8d2fd0bd93a7fed5819a3f07f612e73bc0a41
Owen <owen.allemang@gmail.com>
2020-04-01 17:37:04 +0200

    script to dl sr

--- web_dl.py
+++ web_dl.py
@@ -1,14 +1,16 @@
 import os
 import requests
 from bs4 import BeautifulSoup as BS
-import wget
 
-# DATASET_DIR = os.dir
+DATASET_DIR = os.path.normcase("./dataset")
+if DATASET_DIR != "" and not os.path.exists(DATASET_DIR):
+	os.makedirs(DATASET_DIR)
 
 url = "https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports"
+
 r = requests.get(url, allow_redirects=True)
 
-soup = BS(r.text)
+soup = BS(r.text, features="lxml")
 
 situation_reports_links = []
 available_sr_name = []
@@ -19,9 +21,27 @@ for link in soup.find_all("a"):
 			situation_reports_links.append(link.get("href"))
 	except IndexError:
 		continue
+		
+situation_reports_links = list(dict.fromkeys(situation_reports_links))
 
 for sr in situation_reports_links:
-	available_sr_name.append(sr[52:83])
+	sr_name = situation_reports_links[0].split("/")[-1].split("?")[0]
+	available_sr_name.append(sr_name)
 
+k = 0
 
+for dl_link, name, i in zip(situation_reports_links, available_sr_name, range(len(available_sr_name), -1)):
+	FILE_DIR = os.path.join(DATASET_DIR, name)
+	if not os.path.exists(FILE_DIR):
+		
+		url_dl = "https://who.int" + dl_link
+		report = requests.get(url_dl)
+		
+		with open(FILE_DIR, "wb") as f:
+			f.write(report.content)
+			
+		print("Situation report - ", i+1, "downloaded as ", name)
+		k += 1
+		
+print("%d situation reports downloaded" % k)
 
