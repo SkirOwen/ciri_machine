49ec0f7106a2d30fba5c4e93a5858d3cd6c29e06
Owen <owen.allemang@gmail.com>
2020-04-02 08:53:37 +0200

    imports.py moved location

---
+++ cirilib/imports.py
@@ -0,0 +1,19 @@
+import web_dl
+from cirilib.constants import *
+
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import seaborn as sb
+import pandas as pd
+import prince
+import inspect
+import itertools as it
+import operator as op
+import os
+import random
+import re
+import string
+import sys
+import sklearn as sk
+import tabula
