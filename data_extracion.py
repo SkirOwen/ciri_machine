95c6570a2db2afff7a70103867910ee416e17c69
Owen <owen.allemang@gmail.com>
2020-04-16 11:32:07 +0200

    create process data to get correct line from csv

--- data_extracion.py
+++ data_extracion.py
@@ -12,21 +12,33 @@ def pull_data(selected_data, selected_region, update_selected_countries=True):
         else:
             return
         df = pd.read_csv(url)
-        # process_data
-
+    
     # TODO: Here cases and deaths are on the same csv
     else:  # selected_region == "US"
         data_type = 'deaths' if selected_data == 'Reported Deaths' else 'cases'
         url = url_us_data
         df = pd.read_csv(url)
+    
+    return process_data(df, selected_region)
 
-    row = df.iloc[116, 4:]
-    print(row)
-    row.plot()
-    plt.show()
-    # fig = px.line(df, x=str(df.iloc[0, 4:]), y=df.iloc[1:, 4:], title="test")
-    # fig.show()
 
 def process_data(df, selected_region, *args):
-	dates = []
-	region = df.loc[selected_region]
+    k = 4
+    if selected_region in PROVINCE_REGION:
+        region = df.loc[df["Province/State"] == selected_region]
+    else:
+        region = df.loc[(df["Country/Region"] == selected_region) & (df["Province/State"].isna())]
+        
+        if region.empty:
+            region = df.loc[(df["Country/Region"] == selected_region)].groupby("Country/Region", as_index=False).sum()
+            k = 3
+
+    dates = df.iloc[0:0, k:]
+    
+    # Really ugly, need to rework that better and speed it
+    return pd.DataFrame(data={"dates": list(dates), "region": region.values[0][k:]})
+
+
+if __name__ == "__main__":
+    df = pull_data("Confirmed Cases", "United Kingdom")
+    # sns.lineplot(data=df)
