5d514fc9cb8bda66f660251356290b3d4d4e1f39
Owen <owen.allemang@gmail.com>
2020-04-28 16:03:19 +0200

    requirements.txt added

--- pdf_extraction.py
+++ pdf_extraction.py
@@ -1,26 +1,27 @@
 from cirilib.imports import *
 
 try:
-	import tabula
+    import tabula
 except ImportError:
-	print("Please install tabula for python and try again")
-	
+    print("Please install tabula for python and try again\n",
+          "tabula-py==2.1.0")
+
 
 def export_from_pdf(file_name):
-	file_path = os.path.join(PDF_DIR, file_name)
-	print("Extracting table from", file_name, "...")
-	df = tabula.read_pdf(file_path, pages='all', pandas_options={'header': None})[1:]
-	
-	table_begin_index = 1
-	
-	for i in range(len(df)):
-		if any(word in str(df[i][0][0]) for word in TABLE_START_WORD):
-			table_begin_index = i
-	
-	if len(df) > 1:
-		df = df[0].append(df[table_begin_index:], ignore_index=True)
-	print(file_name, "extracted!")
-	return df
+    file_path = os.path.join(PDF_DIR, file_name)
+    print("Extracting table from", file_name, "...")
+    df = tabula.read_pdf(file_path, pages='all', pandas_options={'header': None})[1:]
+    
+    table_begin_index = 1
+    
+    for i in range(len(df)):
+        if any(word in str(df[i][0][0]) for word in TABLE_START_WORD):
+            table_begin_index = i
+    
+    if len(df) > 1:
+        df = df[0].append(df[table_begin_index:], ignore_index=True)
+    print(file_name, "extracted!")
+    return df
 
 
 # convert to csv, maybe use it?
@@ -31,6 +32,6 @@ def export_from_pdf(file_name):
 files = os.listdir(PDF_DIR)
 
 if __name__ == '__main__':
-	reports = []
-	reports.append(export_from_pdf(files[0]))
-	print(reports)
+    reports = []
+    reports.append(export_from_pdf(files[0]))
+    print(reports)
